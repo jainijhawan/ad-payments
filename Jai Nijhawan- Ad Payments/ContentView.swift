54c10cd5a9430670c8aad1568f63ced2bf2f0b30
Jai Nijhawan <44671239+jainijhawan@users.noreply.github.com>
2025-08-26 11:42:07 +0530

    Initial commit - iOS Ad Payments app

---
+++ Jai Nijhawan- Ad Payments/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Jai Nijhawan- Ad Payments
+//
+//  Created by Jai Nijhawan on 26/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Jai Nijhawan- Ad Payments/Jai_Nijhawan__Ad_PaymentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Jai_Nijhawan__Ad_PaymentsApp.swift
+//  Jai Nijhawan- Ad Payments
+//
+//  Created by Jai Nijhawan on 26/08/25.
+//
+
+import SwiftUI
+
+@main
+struct Jai_Nijhawan__Ad_PaymentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
